05cfb1167a839708a29128a35277546cff654b93
azoladyantyi <aizydyantyi@gmail.com>
2019-10-22 15:35:30 +0200

    made some changes function on script by adding Math.pow

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 function calculate_total() {
-  var purchase_price = document.getElementById("purchase_price").value;
-  var deposit_paid = document.getElementById("deposit_paid").value;
-  var bond_term= document.getElementById("bond_term").value;
-  var fixed_interest = document.getElementById("fixed_interest").value;
+        var purchase_price = document.getElementById("purchase_price").value;
+        var deposit_paid = document.getElementById("deposit_paid").value;
+        var bond_term= document.getElementById("bond_term").value;
+        var fixed_interest = document.getElementById("fixed_interest").value;
+        var result1 = parseFloat((fixed_interest/100/12)*(Math.pow(1+(fixed_interest/100/12),(bond_term*12))));
+        var result2 = parseFloat(Math.pow((1+(fixed_interest/100/12)),(bond_term*12))-1);
+        Math.pow((1+(fixed_interest/100/12)),(bond_term*12))
+        var result3 = parseFloat(purchase_price - deposit_paid);
+        var result = result3 * (result1/result2);
 
-
- var result = (parseFloat(purchase_price - deposit_paid))  * (parseFloat(bond_term*12) *parseFloat(fixed_interest/100));
-  if (result) {
-    document.getElementById("output").innerHTML="The fixed rate mortgage is " +" " + "R" + result;
-  }
-}
+        if (result) {
+          document.getElementById("output").innerHTML="The fixed rate mortgage is " + result;
+        }
+      }
